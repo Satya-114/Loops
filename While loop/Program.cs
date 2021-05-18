13e9c8965b9a6787b10b0d8da64ed84ea31b748a
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 22:08:26 +0530

    Add files via upload

---
+++ While loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("C# For Loop: Iteration {0}", i);
+                i++;
+            }
+        }
+    }
+}
+        
