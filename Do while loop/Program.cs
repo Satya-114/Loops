d24a2312bf48522ceb0438337a07e8233f0c9c18
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 22:13:07 +0530

    Add files via upload

---
+++ Do while loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Loops1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int i = 1, n = 5, product;
+
+			do
+			{
+				product = n * i;
+				Console.WriteLine("{0} * {1} = {2}", n, i, product);
+				i++;
+			} while (i <= 10);
+		}
+	}
+}
+        
