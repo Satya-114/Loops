4e0f4855f02e319fade4ad8d2ac90c1836a48cdb
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 22:26:54 +0530

    Add files via upload

---
+++ For loop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int n = 5, sum = 0;
+
+			for (int i = 1; i <= n; i++)
+			{
+				// sum = sum + i;
+				sum += i;
+			}
+
+			Console.WriteLine("Sum of first {0} natural numbers = {1}", n, sum);
+		}
+	}
+}
+        
