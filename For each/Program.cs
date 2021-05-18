bd181492a1abdeb252edb65816766a9e6aee2b38
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 22:32:21 +0530

    Add files via upload

---
+++ For each/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Loops3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] myArray = { 'H', 'e', 'l', 'l', 'o' };
+
+            foreach (char ch in myArray)
+            {
+                Console.WriteLine(ch);
+            }
+        }
+    }
+}
+        
